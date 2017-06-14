9e58cd1bfa386119d2e59cab07a721abebb18413
Milenko Vorkapic <milenkovorkapic@gmail.com>
2017-06-14 10:54:50 +0200

    TypeScript preparation

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,7 @@ var path = require('path');
 module.exports = {
   entry: {
     '01_intro': './test/01_intro.test.ts',
-    '02_array': './test/02_array.test.ts',
-    '03_collection': './test/03_collection.test.ts',
-    '04_export': './test/04_export.test.ts',
-    '05_queue': './test/05_queue.test.ts',
-    '06_stack': './test/06_stack.test.ts',
-    '07_extra': './test/07_extra.test.ts'
+    '02_array': './test/02_array.test.ts'
   },
   output: {
     filename: '[name].test.js',

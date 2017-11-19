279abc452b3e52270eec48d9cc139acba168140b
Davy Engone <engonenzedavy@yahoo.com>
2017-11-19 23:43:18 +0100

    fixed few issues with solutions

--- src/01_intro.ts
+++ src/01_intro.ts
@@ -6,18 +6,14 @@
 // This first example shows you how to prevent someone from
 // calling this function with something else than numbers
 // Nothing to do here
-export function addNumbers(x: number, y: number): number {
-  return x + y;
-}
+export const addNumbers = (x: number, y: number): number => x + y;
 
 // We might also want to "add" strings:
 // Even though the current implementation is the same and could work for
 // both strings and numbers, the goal of this function is to add strings.
 // By enforcing this in the function signature, we can safely change
 // its implementation later
-export function addStrings(x, y) {
-  return x + y;
-}
+export const addStrings = (x: string, y: string): string  => x + y;
 
 // In this lesson we're going to learn about a couple of Typescript concepts (or
 // type systems in general). Specifically, this is what you'll know as soon as
@@ -35,15 +31,17 @@ export function addStrings(x, y) {
 // This function can accept any type!
 // But it always returns something of the same type as what was provided.
 // Hint: Use a generic type (<T>).
-export function identity(item) {
-  return item;
-}
+export const identity = <T>(item?: T): T => item;
+
 
 // ## attempt
 // attempt applies the passed in function with the supplied arguments. If the
 // function throws an error, the error is being returned. If the function does
 // not throw an error, the result is being returned.
-export function attempt(func, ...args) {
+interface IFunc<T> {
+  (...args: T[]): T
+}
+export function attempt<T>(func: IFunc<T>, ...args: T[]): T | Error {
   try {
     return func(...args);
   } catch(err) {
@@ -52,22 +50,30 @@ export function attempt(func, ...args) {
 }
 
 // ### constant
-// constant returns a function that returns a the passed in value.
-export function constant(value) {
-  return function() {
-    return value;
-  }
+// constant returns a function that returns the passed in value.
+interface IConstant<T>{
+  (): T
 }
+export const constant = <T>(value: T): IConstant<T> => () => value;
 
 // ### noop
 // noop can be called with arbitrary arguments, it will always return
 // `undefined`.
-export function noop() {}
+export const noop = (...args: any[]): undefined => undefined;
 
 // ### times
 // times invokes the passed in iteratee (2nd argument) n times. It returns an
 // array of results.
-export function times(n, iteratee) {
+// declare global {
+//   interface Array<T> {
+//     fill(): Array<T>;
+//   }
+// }
+
+interface IIteratee<T>{
+  (item: number): T;
+}
+export function times<T>(n: number, iteratee: IIteratee<T>): T[] {
   // If the fill function doesn't exist then implement it...
-  return Array(n).fill().map((o, i) => iteratee(i));
+  return Array(n).fill(0).map((o: T, i: number, collection: T[]) => iteratee(i));
 }
